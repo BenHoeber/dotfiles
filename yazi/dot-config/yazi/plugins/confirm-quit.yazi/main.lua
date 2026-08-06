4595345fb538c416f3311c5ed986413c4b4cc5c2
bhoeber <ben.hoeber@sulzmann.energy>
2026-08-06 17:12:01 +0200

    fix(yazi): Properly quit into entry directory.

--- yazi/dot-config/yazi/plugins/confirm-quit.yazi/main.lua
+++ yazi/dot-config/yazi/plugins/confirm-quit.yazi/main.lua
@@ -1,17 +1,33 @@
-local count = ya.sync(function() return #cx.tabs end)
+local count = ya.sync(function()
+    return #cx.tabs
+end)
 
-local function entry()
+local function quit(no_cwd)
+    if no_cwd then
+        ya.emit("quit", { no_cwd_file = true })
+    else
+        ya.emit("quit", {})
+    end
+end
+
+local function entry(_, job)
+    -- `entry_cwd` means: exit without changing the shell directory
+    local no_cwd = job.args[1] == "entry_cwd"
+
     if count() < 2 then
-        return ya.emit("quit", {})
+        return quit(no_cwd)
     end
 
-    local yes = ya.confirm {
+    local yes = ya.confirm({
         pos = { "center", w = 62, h = 10 },
         title = "Quit?",
-        body = ui.Text("There are multiple tabs open. Are you sure you want to quit?"):wrap(ui.Wrap.YES),
-    }
+        body = ui.Text(
+            "There are multiple tabs open. Are you sure you want to quit?"
+        ):wrap(ui.Wrap.YES),
+    })
+
     if yes then
-        ya.emit("quit", {})
+        quit(no_cwd)
     end
 end
 
